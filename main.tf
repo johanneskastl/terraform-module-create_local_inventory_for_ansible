17e70ffa9c0f60c0f36b6e7b52baaa646ca9083a
Johannes Kastl <kastl@b1-systems.de>
2020-04-17 15:34:12 +0200

    main.tf: fix template syntax to make 'terraform fmt' happy

--- main.tf
+++ main.tf
@@ -2,17 +2,17 @@
 
 data "template_file" "ansible_inventory_template" {
   template = <<EOT
-%{ for group in var.groups_to_add ~}
-[${ group.name }]
-%{ for fip_association in group.instancesobject.floating_ip_associations.* ~}
-%{ for host in group.instancesobject.instances.* ~}
-%{ if host.id == fip_association.instance_id ~}
+%{for group in var.groups_to_add~}
+[${group.name}]
+%{for fip_association in group.instancesobject.floating_ip_associations.*~}
+%{for host in group.instancesobject.instances.*~}
+%{if host.id == fip_association.instance_id~}
 ${host.name} ansible_user=${group.user_for_ansible} ansible_host=${fip_association.floating_ip} external_ip=${fip_association.floating_ip} internal_ip=${host.network[0].fixed_ip_v4}
-%{ endif ~}
-%{ endfor ~}
-%{ endfor ~}
+%{endif~}
+%{endfor~}
+%{endfor~}
 
-%{ endfor ~}
+%{endfor~}
 EOT
 }
 
